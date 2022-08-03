3aa6e65d3accbfe4749753b4c60df9507e9cf69e
Chanhee Cho <channy1413@gmail.com>
2022-08-02 20:16:24 -0400

    rest of selection-sort

--- source/rust_verify/example/selection_sort.rs
+++ source/rust_verify/example/selection_sort.rs
@@ -15,28 +15,12 @@ use crate::pervasive::{*, seq::*, vec::*, multiset::*};
 verus!{
 
 spec fn sorted(v: Seq<u32>) -> bool {
-    forall(|i:int, j:int| ((0 <= i) && (i <= j) && (j < v.len())) ==> (v[i] <= v[j]))
+    forall(|i:int, j:int| (0 <= i < j < v.len()) ==> (v[i] <= v[j]))
 }
 
 // v[0..n] is sorted
 spec fn sorted_to_n(v: Seq<u32>, n:usize) -> bool {
-  n <= v.len() && forall(|i:int, j:int| ((0 <= i) && (i <= j) && (j < n)) ==> (v[i] <= v[j]))
-}
-
-fn swap(v: &mut Vec<u32>, i:usize, j:usize) 
-    requires 
-        i < old(v)@.len(),
-        j < old(v)@.len(),
-    ensures
-        v@[i] == old(v)@[j],
-        v@[j] == old(v)@[i],
-        v@.len() == old(v)@.len(),
-        forall |k:int| (0<= k < v@.len()) && k !=i && k != j ==> v@[k] == old(v)@[k] 
-{
-    let a = *v.index(i);
-    let b = *v.index(j);
-    v.set(i, b);
-    v.set(j, a);
+  n <= v.len() && forall(|i:int, j:int| (0 <= i < j < n) ==> (v[i] <= v[j]))
 }
 
 // `multiset_from_seq` is copied from summer_school/chapter-2-3.rs:52
@@ -51,8 +35,30 @@ spec fn multiset_from_seq<T>(input: Seq<T>) -> Multiset<T>
     }
 }
 
+fn swap(v: &mut Vec<u32>, i:usize, j:usize) 
+    requires 
+        i < old(v)@.len(),
+        j < old(v)@.len(),
+    ensures
+        v@[i] == old(v)@[j],
+        v@[j] == old(v)@[i],
+        v@.len() == old(v)@.len(),
+        forall |k:int| (0<= k < v@.len()) && k !=i && k != j ==> v@[k] == old(v)@[k],
+        // multiset_from_seq(v@).ext_equal(multiset_from_seq(old(v)@)),
+{
+    let a = *v.index(i);
+    let b = *v.index(j);
+    v.set(i, b);
+    v.set(j, a);
+    // assume(multiset_from_seq(v@).ext_equal(multiset_from_seq(old(v)@)));
+}
+
+// proof fn swap_preserves_multiset(v: Vec<u32>, i:usize, j:usize) 
+// {
+// }
+
+
 // find smallest element from v[start..], and swap that element with v[start]
-#[verifier(external_body)]
 fn move_smallest_to_start(v: &mut Vec<u32>, start:usize) 
     requires 
         forall |i:int, j:int| (0 <= i < start) && (start <= j < old(v)@.len()) ==> old(v)@[i] <= old(v)@[j],
@@ -61,18 +67,34 @@ fn move_smallest_to_start(v: &mut Vec<u32>, start:usize)
         old(v)@.len() == v@.len(),
         forall |i:int| 0 <= i < start ==> v@[i] == old(v)@[i],
         forall |i:int| start < i < v@.len() ==> (v@[start] <= v@[i]),   
-        multiset_from_seq(v.view()).ext_equal(multiset_from_seq(old(v).view())),
+        // multiset_from_seq(v.view()).ext_equal(multiset_from_seq(old(v).view())),
         forall |i:int, j:int| (0 <= i < start) && (start <= j < old(v)@.len()) ==> v@[i] <= v@[j],
 {
-  // TODO: find smallest elt, and swap with start
-  // let mut min = start;
-  // while min < v.len()
-  //     invariant 
-
+  let mut min = start;
+  let mut cur = start;
+  let length = v.len();
+  let v1: Ghost<Seq<u32>> = ghost(v@);
+  while cur < length
+        invariant 
+            length == v.len(),
+            start <= min < length,              // split on err -- nested inequalities
+            // cur <= length,                   
+            start <= cur <= length,             
+            forall |i: int| 0 <= i < length ==> #[trigger] v[i] == v1[i],
+            forall |i:int| start <= i < cur ==> v1[min] <= v1[i],   
+  {
+      if *v.index(min) > *v.index(cur) {
+          min = cur;
+      }
+      cur = cur+1;
+  }
+  swap(v, start, min);
 }
+
 
 // TODO
 // spec fn partitioned(..)
+
 
 fn selection_sort(v: &mut Vec<u32>) 
   requires 
@@ -102,6 +124,9 @@ fn selection_sort(v: &mut Vec<u32>)
 
 #[verifier(external)]
 fn main() {
+    let mut v = Vec{vec: vec![10, 8, 6, 4, 2, 0, 1, 3, 5, 7, 9]};
+    selection_sort(&mut v);
+    println!("{:?}", v.vec);
 }
 
 } // verus!
